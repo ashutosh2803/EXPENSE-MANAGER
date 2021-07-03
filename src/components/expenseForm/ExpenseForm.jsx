28da51d22b9bdb6bf1c14b6025f601ddf6511284
unknown <ashuisalive@gmail.com>
2021-07-03 15:41:31 +0530

    initial commit

---
+++ src/components/expenseForm/ExpenseForm.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import styles from "./ExpenseForm.module.css";
+
+const ExpenseForm = () => {
+    
+    return (
+        <div>
+            <form>
+                <label></label>
+                <input />
+                <label></label>
+                <input/>
+            </form>
+        </div>
+    )
+}
+
+export { ExpenseForm };
---
+++ src/components/homepage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ExpenseForm } from '../expenseForm/ExpenseForm';
+import styles from "./HomePage.module.css";
+
+const HomePage = () => {
+    return (
+        <div>
+            <ExpenseForm/>
+        </div>
+    )
+}
+
+export {HomePage};
