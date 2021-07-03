43846800fc79450625e71b878a3d970e51d96f8e
unknown <ashuisalive@gmail.com>
2021-07-04 00:49:38 +0530

    updated expense form

--- src/components/expenseForm/ExpenseForm.jsx
+++ src/components/expenseForm/ExpenseForm.jsx
@@ -1,16 +1,31 @@
 import React, {useState} from 'react';
 import styles from "./ExpenseForm.module.css";
+import { v4 as uuid } from "uuid";
 
 const ExpenseForm = () => {
-    
+    const [title, setTitle] = useState("");
+    const [amount, setAmount] = useState(0);
+    const [type, setType] = useState("");
+
+    const handleClick = () => {
+        const payload = {
+            id: uuid(),
+            title,
+            amount,
+            type
+        }
+        
+    }
     return (
         <div>
-            <form>
-                <label></label>
-                <input />
-                <label></label>
-                <input/>
-            </form>
+            <label htmlFor="title">Title</label>
+            <input type="text" value={title} required="true" name="amount" onChange={(e) => setTitle(e.target.value)} />
+            <label htmlFor="amount">Amount</label>
+            <input type="number" value={amount} name="amount" required="true" onChange={(e) => setAmount(e.target.value)} />
+            <label htmlFor="type">Type</label>
+            <input type="radio" name="type" value="credit" />
+            <input type="radio" name="type" value="debit" />
+            <button onClick={handleClick}>SUBMIT</button>
         </div>
     )
 }
