2460675f8862b72e9d18aa5d8eb5cd06e5b27afb
unknown <ashuisalive@gmail.com>
2021-07-04 18:36:54 +0530

    expenseForm working

--- src/components/expenseForm/ExpenseForm.jsx
+++ src/components/expenseForm/ExpenseForm.jsx
@@ -3,29 +3,38 @@ import styles from "./ExpenseForm.module.css";
 import { v4 as uuid } from "uuid";
 
 const ExpenseForm = () => {
+    const [details, setDetails] = useState([]);
     const [title, setTitle] = useState("");
     const [amount, setAmount] = useState(0);
     const [type, setType] = useState("");
 
     const handleClick = () => {
+        console.log(type)
         const payload = {
             id: uuid(),
             title,
             amount,
             type
         }
-        
+        console.log(payload)
+        setDetails([...details, payload]);    
+        console.log("updated Details",details);
     }
     return (
         <div>
             <label htmlFor="title">Title</label>
-            <input type="text" value={title} required="true" name="amount" onChange={(e) => setTitle(e.target.value)} />
+            <input type="text" placeholder="title here" value={title} required={true} name="amount" onChange={(e) => setTitle(e.target.value)} /><br/>
             <label htmlFor="amount">Amount</label>
-            <input type="number" value={amount} name="amount" required="true" onChange={(e) => setAmount(e.target.value)} />
-            <label htmlFor="type">Type</label>
-            <input type="radio" name="type" value="credit" />
-            <input type="radio" name="type" value="debit" />
-            <button onClick={handleClick}>SUBMIT</button>
+            <input type="number" value={amount} name="amount" required={true} onChange={(e) => setAmount(e.target.value)} /><br/>
+            <div style={{}}>
+                <p>Type</p>
+                <select onChange={(e) => setType(e.target.value)}>
+                    <option value="credit">Credit</option>
+                    <option value="debit">Debit</option>
+                </select>
+            </div>
+            <br/>
+            <button onClick={() => handleClick()}>SUBMIT</button>
         </div>
     )
 }
