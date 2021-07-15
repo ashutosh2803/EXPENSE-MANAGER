9c9c6ff271583e357964faf8bcd873f096f807d6
unknown <ashuisalive@gmail.com>
2021-07-15 11:34:30 +0530

    completed alignment of components with profile box changes

--- src/components/profileBox/ProfileBox.jsx
+++ src/components/profileBox/ProfileBox.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./ProfileBox.module.css";
 
 const ProfileBox = () => {
+    let [time, setTime] = useState("");
+    let [date, setDate] = useState("");
+    useEffect(() => {
+        let timer = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+            setDate(new Date().toDateString());
+        })
+        return (() => {
+            clearInterval(timer);
+        })
+    },[])
     return (
         <div className={styles.profileWrapper}>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpjaO_dMO8xLZPOkLuzIOODumnXLo5qdhS-A&usqp=CAU" alt="profilePic" />
-            <p>Ashutosh Kumar</p>
+            <p className={styles.name}>Ashutosh Kumar</p>
+            <p>{date}</p>
+            <p style={{padding: "2%"}}>{time}</p>
         </div>
     )
 }
