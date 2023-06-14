b395f0eca04c8549826056e5637a03cbf47a85c9
unknown <ashuisalive@gmail.com>
2023-06-14 10:31:45 +0530

    removed app css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route } from 'react-router-dom';
-import './App.css';
 import { HomePage } from './components/homepage/HomePage';
 import { LoginPage } from './components/loginPage/LoginPage';
 
